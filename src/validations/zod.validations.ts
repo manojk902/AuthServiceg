a5619f3802affe2f59a3379faa8d1daa9d0a441e
Mukesh Kumar <mukesh.scroffice@gmail.com>
2025-10-15 15:10:53 +0530

    change zod validation for phone number

--- src/validations/zod.validations.ts
+++ src/validations/zod.validations.ts
@@ -64,7 +64,8 @@ export const updateUserValidationSchema = z.object({
   last_name: z.string(),
   email: z.string().min(1, { message: "Email is required" }).email({ message: "Invalid email format" }),
   recovery_email: z.string().default("recovery@example.com"),
-  phone_number: z.coerce.string().default("9xxxxxxxxx"),
+  phone_number: z.coerce.string().regex(/^[6-9]\d{9}$/, "Phone number must start with 6, 7, 8, or 9 and be 10 digits long")
+  .default("9xxxxxxxxx")
 })
 
 // -----------------------------------------------UPDATE USER INFO VALIDATION
